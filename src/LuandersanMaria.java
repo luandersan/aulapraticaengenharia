5edd5f865e8d9b5f269501b76f5258939c65221d
ewerton.guedes <ewerton.guedes@LAB2-DCX-22.dcx.ufpb.br>
2017-09-11 11:43:10 -0300

    Ewerton Editou

--- src/LuandersanMaria.java
+++ src/LuandersanMaria.java
@@ -1,7 +1,8 @@
 
 public class LuandersanMaria {
 	public static void main(String[] args) {  
-	     System.out.println("Eu Editei aqui - Thiago Vasconcelos"); 
+	     System.out.println("Eu Editei aqui - Thiago Vasconcelos");
+	     System.out.println("Ewerton editou"); 
 	   }  
 	
 
